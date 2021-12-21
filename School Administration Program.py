07a09498906f01eaf0a5e74d469df4679d12d3f8
Abhishek Thatithuru <82454618+T-Abhishek@users.noreply.github.com>
2021-12-21 22:28:53 +0530

    Update School Administration Program.py

--- School Administration Program.py
+++ School Administration Program.py
@@ -1,39 +1,23 @@
 import csv
 import os
-def file_found(file_path):
-    #this function returns 
-    return os.path.exists(file_path)
 
-def write_into_csv1(info_list):
-    #this function is for writing into csv with headers if file not created
-    with open('student_info.csv','a',newline='') as csv_file:
-            writer=csv.writer(csv_file)
-            writer.writerow(['NAME','AGE','CONTACT NO','EMAIL ID'])
-            writer.writerow(student_info_list)
-            
-def write_into_csv2(info_list):
-    #this function for writing into csv file if file already exists
-    with open('student_info.csv','a',newline='') as csv_file:
-            writer=csv.writer(csv_file)
-            writer.writerow(student_info_list)
+def write_into_csv(info_list):
+    if os.path.exists('student_info.csv')==True:
+        with open('student_info.csv','a',newline='') as sfile:
+            parser=csv.writer(sfile)
+            parser.writerow(s_info_list)
+    else:
+        with open('student_info.csv','a',newline='') as sfile:
+            parser=csv.writer(sfile)
+            parser.writerow(['NAME','AGE','CONTACT NO','EMAIL ID'])
+            parser.writerow(s_info_list)
 
 condition=True
-while(condition):
-    student_info=input("Enter Student information in the following format(Name Age Contact_Number E-mail_ID):")
-    print("Entered inforamtion: "+student_info)
-    #funtion to s
-    student_info_list=student_info.split(' ')
+while condition:
+    student_input=input("Enter Student information in the following format(Name Age Contact_Number E-mail_ID):")
+    s_info_list=student_input.split(' ')
+    write_into_csv(s_info_list)
 
-    #condition for writing data into csv file
-    if file_found('student_info.csv')==True:
-        print('file found')
-        #funtion call
-        write_into_csv2(student_info_list)
-    else:
-        #funtion call
-        write_into_csv1(student_info_list)
-        
-    #user input for adding more student data
     while True:
         condition_check=input("Enter (yes/no) if you want to enter information for another student:")
         if condition_check=='yes':
@@ -43,5 +27,5 @@ while(condition):
             condition=False
             break
         else:
-            print('Invalid input! Please try again')
+            print('enter only yes/no!')
             continue
